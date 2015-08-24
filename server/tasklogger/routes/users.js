25a532b410209d8fcd802a1b2d0b1f46902dfb68
Jiyin He <jiyinhe@139-80.ch.bbc.co.uk>
2015-08-24 14:39:38 +0100

    To do list initial commit

--- server/tasklogger/routes/users.js
+++ server/tasklogger/routes/users.js
@@ -4,7 +4,11 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
-    res.redirect('/users/login');
+    if (req.user===undefined){
+        res.redirect('/users/login');
+    }
+    else 
+        res.redirect('/users/dashboard');
 });
 
 /* GET registration page. */
@@ -75,9 +79,14 @@ router.post('/register_user', function(req, res){
 
 /* GET registration page. */
 router.get('/dashboard', function(req, res, next) {
+    if (req.user===undefined){
+        res.redirect('/users/login');
+    }
     //console.log(req.user)
     res.render('dashboard', {
-        "user": req.user
+        "user": req.user,
+        "homeclass": "active",
+        "title": "Tasklog - Home",
     });
 });
 
@@ -95,6 +104,146 @@ router.post('/checkid', function(req, res){
          else{
             res.send({'err': false, 'user': docs[0]});
          }
+    });
+});
+
+/* Instructions page */
+router.get('/instructions', function(req, res, next) {
+    //console.log(req.user)
+    res.render('instructions', {
+        "user": req.user,
+        "instructionclass": "active",
+        "title": "Tasklog - Instructions",
+    });
+});
+
+
+/* To-do page */
+router.get('/mytodo', function(req, res, next) {
+    if (req.user===undefined){
+        res.redirect('/users/login');
+    }
+    //Get all tasks
+    var collection = req.db.get('user_tasks');
+    //first get all the parents
+    collection.find({'userid': req.user.userid, 'task_level': 0}, 
+        {sort: {timestamp: 1}}, function(e, docs){
+        var tasks_today = [];
+        var tasks_past = [];
+        var today = (new Date()).setHours(0,0,0,0);
+
+        var parents = {}
+        for (var i = 0; i<docs.length; i++){
+            var task_status = '';
+            if (docs[i].done)
+                task_status = 'task-done';
+            var d = {'taskid': docs[i]['_id'],
+                    'task': docs[i]['task'],
+                    'subtasks': [],
+                    'refresh': docs[i]['refresh'],
+                    'status': task_status,
+                }
+            parents[d.taskid] = d;
+        }
+        collection.find({'userid': req.user.userid, 'task_level': 1}, 
+            {sort:{timestamp: 1}}, function(e, docs){
+                for(var i = 0; i<docs.length; i++){
+                    var task_status = '';
+                    if (docs[i].done)
+                        task_status = 'task-done';
+ 
+                    var d = {'taskid': docs[i]['_id'], 
+                            'task': docs[i]['task'],
+                            'task': task_status,
+                        };
+                    parents[docs[i]['parent_task']].subtasks.push(d);
+                }
+            //seperate today's task and past tasks
+            for (var key in parents){
+                if (parents[key]['refresh'] > today)
+                   tasks_today.push(parents[key]);
+                else
+                   tasks_past.push(parents[key]);
+            }
+            tasks_today.sort(function(a, b){return  a.refresh - b.refresh;});
+            tasks_past.sort(function(a, b){return  a.refresh - b.refresh;});
+
+            res.render('todo', {
+                "user": req.user,
+                "todoclass": "active",
+                "title": "Tasklog - My todo list",
+                "tasks_today": tasks_today,
+                "tasks_past": tasks_past,
+            });
+        });  
+    }); 
+});
+
+router.post('/submit_todo', function(req, res){
+    var collection = req.db.get('user_tasks');
+    if (req.body.event == 'add_task'){
+        //refresh: when add a past task to today, 
+        //refresh is set to today while create_time is not changed
+        var create_time = parseInt(req.body.create_time);
+        var entry = {
+            'userid': req.user.userid,
+            'time_created': create_time,
+            'time_done': 0,
+            'task': req.body.task,
+            'task_level': 0,
+            'parent_task': 0,
+            'done': false,
+            'refresh': create_time,
+        }
+        collection.insert(entry, function(err, doc){
+            if (err){
+                console.log('DB error: ' + err)
+                res.send({'err': true, 'emsg': err})
+            }
+            else
+                res.send({'err': false, 'task': doc})
+        });
+    }
+    else if (req.body.event == 'task_status_change'){
+        console.log('here');
+        if (req.body.is_main_task)
+            console.log('main-task')
+        //if a task is done, all its unfinished subtasks are done
+        
+        //if a tasks is undone, all the subtasks that were done at the
+        // same time are undone 
+        // if a subtask is done, only change its own status
+        // if a subtask is undone, its parent becomes undone
+ 
+    }
+});
+
+/* Annotation page */
+router.get('/annotation', function(req, res, next) {
+    if (req.user===undefined){
+        res.redirect('/users/login');
+    }
+ 
+    //console.log(req.user)
+    res.render('annotation', {
+        "user": req.user,
+        "annotationclass": "active",
+        "title": "Tasklog - My annotations",
+    });
+});
+
+
+/* Account information page */
+router.get('/account', function(req, res, next) {
+    if (req.user===undefined){
+        res.redirect('/users/login');
+    }
+ 
+    //console.log(req.user)
+    res.render('account', {
+        "user": req.user,
+        "accountclass": "active",
+        "title": "Tasklog - My account", 
     });
 });
 
